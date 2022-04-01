495f6b8de45eb651ffe242c1ee084c09377d2be3
jay chaudhari <jpchaudhari@myseneca.ca>
2022-04-01 02:40:03 -0400

    MS4! code written needs to debug

---
+++ ms4/ms4/Perishable.h
@@ -0,0 +1,24 @@
+#ifndef SDDS_PERISHABLE_H
+#define SDDS_PERISHABLE_H
+#include "Item.h"
+#include "Date.h"
+namespace sdds
+{
+	class Perishable:public Item
+	{
+		Date m_expiry;
+		char* m_instructions{};
+	public:
+		Perishable();
+		Perishable(const Perishable& p);
+		Perishable& operator=(const Perishable& p);
+		~Perishable();
+		const Date& expiry()const;
+		virtual int readSku(std::istream& istr);
+		virtual std::ofstream& save(std::ofstream& ofstr)const;
+		virtual std::ifstream& load(std::ifstream& ifstr);
+		virtual std::ostream& display(std::ostream& ostr)const;
+		virtual std::istream& read(std::istream& istr);
+	};
+}
+#endif // !SDDS_PERISHABLE_H
